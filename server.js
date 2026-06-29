4dc24be1d6eb8637daaf34ab3be2879baa28d202
kingwinny43-crypto <kingwinny43@gmail.com>
2026-06-29 17:15:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,24 +6,83 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let coins = 0;
+let users = {};
 
+// Home
 app.get("/", (req, res) => {
     res.send("Backend is running!");
 });
 
-app.get("/coins", (req, res) => {
+// Login
+app.post("/login", (req, res) => {
+    const { username } = req.body;
+
+    if (!users[username]) {
+        users[username] = {
+            coins: 0
+        };
+    }
+
     res.json({
-        coins: coins
+        username,
+        coins: users[username].coins
     });
 });
 
-app.post("/watch-ad", (req, res) => {
-    coins += 50;
+// Get user
+app.get("/user/:username", (req, res) => {
+    const username = req.params.username;
+
+    if (!users[username]) {
+        users[username] = {
+            coins: 0
+        };
+    }
+
+    res.json(users[username]);
+});
+
+// Add coins
+app.post("/add-coins", (req, res) => {
+    const { username, coins } = req.body;
+
+    if (!users[username]) {
+        users[username] = {
+            coins: 0
+        };
+    }
+
+    users[username].coins += coins;
 
     res.json({
         success: true,
-        coins: coins
+        coins: users[username].coins
+    });
+});
+
+// Exchange
+app.post("/exchange", (req, res) => {
+    const { username } = req.body;
+
+    if (!users[username]) {
+        return res.json({
+            success: false,
+            message: "User not found"
+        });
+    }
+
+    if (users[username].coins < 1000) {
+        return res.json({
+            success: false,
+            message: "Need 1000 coins"
+        });
+    }
+
+    users[username].coins -= 1000;
+
+    res.json({
+        success: true,
+        message: "Exchange successful!"
     });
 });
 
